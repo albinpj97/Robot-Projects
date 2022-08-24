81ef165f68f61cd5a844ae6ca366e8a92db7c1fd
albinpj97 <49243119+albinpj97@users.noreply.github.com>
2022-08-24 11:19:21 +0530

    Delete OrangeHrAutomation directory

--- OrangeHrAutomation/OrangeHrAutomation.bat
+++
@@ -1,3 +0,0 @@
-@ECHO OFF
-CALL cd /D %1
-rcc run -e devdata/env.json --task  Run_Orange_HR_Process
--- OrangeHrAutomation/orange_hr_run.bat
+++
@@ -1,3 +0,0 @@
-@ECHO OFF
-CALL cd C:\robot_run\OrangeHrAutomation
-rcc run -e devdata/env.json --task  Run_Orange_HR_Process
